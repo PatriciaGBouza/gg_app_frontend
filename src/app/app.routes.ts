aeed2bc43325c1caa2c00c91bc4358dd42cf851d
Angie <82223578+ILMPI@users.noreply.github.com>
2024-05-26 18:29:42 +0200

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { HomeComponent } from './components/home/home.component';
 import { GroupsComponent } from './components/groups/groups.component';
 import { ExpensesComponent } from './components/expenses/expenses.component';
 import { BalanceComponent } from './components/balance/balance.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 export const routes: Routes = [
 { path: '', pathMatch:'full', redirectTo:'home' },
@@ -10,6 +12,8 @@ export const routes: Routes = [
 { path: 'groups', component: GroupsComponent},
 { path: 'expenses', component: ExpensesComponent},
 { path: 'balance', component: BalanceComponent},
+{ path: 'sign-in', component: SignInComponent },
+{ path: 'sign-up', component: SignUpComponent },
 { path: '**', redirectTo:'home'}
 
 ];
