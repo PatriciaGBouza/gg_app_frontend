ac954fd97517f65f29bbcc6cc9ee867ef439ee66
PatriciaGBouza <patricia.gonzalez.bouza@gmail.com>
2024-06-08 17:53:55 +0200

    Create groups and home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,17 @@ import { HomeComponent } from './components/home/home.component';
 import { GroupsComponent } from './components/groups/groups.component';
 import { ExpensesComponent } from './components/expenses/expenses.component';
 import { BalanceComponent } from './components/balance/balance.component';
+import { GroupFormComponent } from './components/group-form/group-form.component';
 
 export const routes: Routes = [
 { path: '', pathMatch:'full', redirectTo:'home' },
 { path: 'home', component: HomeComponent},
 { path: 'groups', component: GroupsComponent},
+{ path: 'newgroup', component: GroupFormComponent},
+{ path: 'editgroup/:id', component: GroupFormComponent},
 { path: 'expenses', component: ExpensesComponent},
-{ path: 'balance', component: BalanceComponent},
-{ path: '**', redirectTo:'home'}
+{ path: 'balance', component: BalanceComponent}
+/*,
+{ path: '**', redirectTo:'home'}*/
 
 ];
--- src/app/components/groups/groups.component.ts
+++ src/app/components/groups/groups.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component , inject} from '@angular/core';
+import {MatTableModule} from '@angular/material/table';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import { RouterLink } from '@angular/router';
+import { GroupsService } from '../../services/groups.service';
+import { IGroup } from '../../interfaces/igroup.interface';
+import { IUser } from '../../interfaces/iuser.interface';
+
 
 @Component({
   selector: 'app-groups',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, MatTableModule,MatButtonModule, MatMenuModule, MatIconModule],
   templateUrl: './groups.component.html',
   styleUrl: './groups.component.css'
 })
-export class GroupsComponent {
 
+export class GroupsComponent {
+  groupsService= inject(GroupsService);
+  displayedColumns: string[] = ['name', 'description', 'options'];
+  groupsInfo: IGroup[]|any;
+
+  user: IUser={ id: 1};
+
+  ngOnInit(){
+      this.groupsInfo=this.groupsService.getAllGroupsByUser(this.user);
+    console.log(this.groupsInfo);
+  }
+
+ /* ngOnInit() {
+    this.groupsService.getAllGroups().subscribe() => {
+        console.log("groupsService.getAllGroups returned "+ JSON.stringify(data));
+        this.groupsInfo=data;
+        
+    });
+  }*/
 }
